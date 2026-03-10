6f9e144cf18871a18fc003801ba20a47ec18bd21
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2026-03-09 23:51:35 -0300

    Delete directory

--- hentaiProject/hentai.py
+++
@@ -1,6 +0,0 @@
-class Hentai():
-    def yamete(args):
-        print(args)
-
-    def nisan(args):
-        print(eval(str(args)))
--- hentaiProject/main.py
+++
@@ -1,5 +0,0 @@
-import hentai
-hentai = hentai.Hentai
-
-hentai.yamete("Oni-chan")
-hentai.nisan(6 + 9)
